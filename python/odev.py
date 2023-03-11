652953438d1605db9d94a6cbf4b672b7d7416bb8
tugcekrgz <tugcekrgz28@gmail.com>
2023-03-11 13:29:15 +0300

    python odevim
    kodlama.io sitesinden aldığım kursun birinci ödevi

---
+++ python/odev.py
@@ -0,0 +1,31 @@
+#PYTHON VERİ TİPLERİ 
+
+# 1.SORU
+
+#string(str) = karakter dizisi,metinsel ifade => tek tırnak veya çift tırnak içerisinde yazılır.
+#integer(int)= tam sayıları ifade eder.
+#float=ondalıklı sayıları ifade eder.
+#boolen=kullanıcıdan alınan veya yapılan işlemin doğruluk değerini gösterir => (True,False)
+
+#2.SORU 
+
+adSoyad="Tuğçe Karagöz "  #str
+yas=19 #int
+kilo=62.30 #float
+
+#3.SORU 
+
+sinav1=50
+sinav2=65
+ort=(sinav1+sinav2)/2
+
+# ortlaması 50 ve üzeri ise sınıfı geçecek 
+if ort>50:
+    print("Sınıfı Başarıyla Geçtiniz.")
+elif ort==50:
+    print("Sınıfı Geçtiniz.") 
+else:
+    print("Maalesef Sınıfı Geçemediniz.") 
+
+
+    
